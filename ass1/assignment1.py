d6b7ff569c28b4b581ae681cd1853ad82d4f31f5
Chau Thanh Dat <chauthanhdat1999@gmail.com>
2021-03-25 03:45:33 +0700

    clean code

--- ass1/assignment1.py
+++ ass1/assignment1.py
@@ -12,11 +12,8 @@ import pygame.event
 import pygame.mixer
 import random
 
-class Axe():
-    pass
-
 class Zombie(pygame.sprite.Sprite):
-    def __init__(self, pos, idx):
+    def __init__(self, idx):
         super().__init__()
         self.sprites = []
         self.sprites.append(pygame.image.load('data/zombie_0.png'))
@@ -37,8 +34,8 @@ class Zombie(pygame.sprite.Sprite):
 
         self.image = self.sprites[self.current_sprite]
         self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.rect.x = AVAILABLE_POS[idx][0]
+        self.rect.y = AVAILABLE_POS[idx][1]
     
     def update(self):
         if self.isDie:
@@ -51,27 +48,29 @@ class Zombie(pygame.sprite.Sprite):
                 self.image = self.sprites[self.current_sprite]
 
 class Hit(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, idx):
         super().__init__()
         self.image = pygame.image.load('data/hit.png')
         self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.rect.x = AVAILABLE_POS[idx][0]
+        self.rect.y = AVAILABLE_POS[idx][1]
         self.spawn_time = pygame.time.get_ticks()
-
+        
 class GameState():
     def __init__(self):
         self.state = 'intro'
         self.score = 0
         self.miss = 0
-        self.bg_img = pygame.image.load('data/bg.png')
-        self.axe_img = pygame.image.load('data/axe.png')
-        self.zombie_idx = -1
         self.available_idx = [True,True,True,True,True,True,True,True,True]
-        self.whack_sound = pygame.mixer.Sound('data/whack.wav')
-        self.hit_sound = pygame.mixer.Sound('data/kick.wav')
         self.zombie_spawn_time = 3000
         self.prev_zombie_spawn_time = 0
+
+        self.bg_img = pygame.image.load('data/bg.png')
+        self.axe_img = pygame.image.load('data/axe.png')
+        
+        self.whack_sound = pygame.mixer.Sound('data/whack.wav')
+        self.hit_sound = pygame.mixer.Sound('data/kick.wav')
+        
 
     def intro(self):
         for event in pygame.event.get():
@@ -80,7 +79,10 @@ class GameState():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.state = 'main_game'
-                self.prev_zombie_spawn_time += pygame.time.get_ticks()
+                self.prev_zombie_spawn_time = pygame.time.get_ticks() - 2000
+
+                pygame.mixer.music.load('data/bg.wav')
+                pygame.mixer.music.play(-1)
 
         # <Drawing>
         ready_text = pixeboy_font.render('Ready ?', False, WHITE)
@@ -105,7 +107,7 @@ class GameState():
             while not self.available_idx[random_idx]:
                 random_idx = random.randint(0,8)
             self.available_idx[random_idx] = False
-            zombie_sprites.add(Zombie(available_pos[random_idx], random_idx))
+            zombie_sprites.add(Zombie(random_idx))
 
             self.prev_zombie_spawn_time = current_time
 
@@ -123,6 +125,8 @@ class GameState():
                 elif current_time - _.spawn_time >= ZOMBIE_LIFE_CYCLE:
                     _.isDie = True
                     self.miss += 1
+                    if self.miss >= 10:
+                        self.state = 'play_again'
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -141,7 +145,7 @@ class GameState():
                             _.isDie = True
                             self.score += 1
                             self.hit_sound.play()
-                            hit_sprites.add(Hit((_.rect.x, _.rect.y)))
+                            hit_sprites.add(Hit(_.index))
 
             if event.type == pygame.MOUSEBUTTONUP:
                 self.axe_img = pygame.image.load('data/axe.png')
@@ -163,15 +167,46 @@ class GameState():
 
         pygame.display.flip()
         # </Drawing>
+    
+    def play_again(self):
+        pygame.mouse.set_visible(True)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.state = 'main_game'
+                    self.score = 0
+                    self.miss = 0
+                    self.available_idx = [True,True,True,True,True,True,True,True,True]
+                    self.zombie_spawn_time = 3000
+                    self.prev_zombie_spawn_time = pygame.time.get_ticks() - 2000
+
+                    zombie_sprites.empty()
+                    hit_sprites.empty()
+
+        # <Drawing>
+        ready_text = pixeboy_font.render('PLAY AGAIN', False, WHITE)
+        text_rect = ready_text.get_rect(center=[screen_w/2, screen_h/2])
+        screen.blit(ready_text, text_rect)
+
+        pygame.display.flip()
+        # </Drawing>
+
 
     def state_manager(self):
         if self.state == 'intro':
             self.intro()
         if self.state == 'main_game':
             self.main_game()
+        if self.state == 'play_again':
+            self.play_again()
 
 def draw_text(text, dest):
     screen.blit(pixeboy_font.render(text, False, WHITE), dest)
+
 
 pygame.init()
 
@@ -180,25 +215,25 @@ screen_w = 1280
 screen_h = 720
 size = (screen_w,screen_h)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Whack A Zombie')
-pygame.display.set_icon(pygame.image.load('data/icon.png'))
+pixeboy_font = pygame.font.Font('data/Pixeboy.ttf', 80)
 clock = pygame.time.Clock()
 
-ZOMBIE_LIFE_CYCLE = 3000
-HIT_LIFE_CYCLE = 300
+pygame.display.set_caption('Whack A Zombie')
+pygame.display.set_icon(pygame.image.load('data/icon.png'))
 
-# Define Color
+# <Define>
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
+ZOMBIE_LIFE_CYCLE = 2000
+HIT_LIFE_CYCLE = 200
+AVAILABLE_POS = [(385,125),(601,125),(817,125),(385,341),(601,341),(817,341),(385,557),(601,557),(817,557)]
+# </Define>
+
 game_state = GameState()
+
 zombie_sprites = pygame.sprite.Group()
 hit_sprites = pygame.sprite.Group()
-available_pos = [(385,125),(601,125),(817,125),(385,341),(601,341),(817,341),(385,557),(601,557),(817,557)]
-pixeboy_font = pygame.font.Font('data/Pixeboy.ttf', 80)
-
-pygame.mixer.music.load('data/bg.wav')
-pygame.mixer.music.play(-1)
 
 while True:
     game_state.state_manager()
