bf25785ec6f31d41ba28018476703dbe55e6502e
Chau Thanh Dat <chauthanhdat1999@gmail.com>
2021-04-01 15:52:27 +0700

    edge detect and reflect true

---
+++ ass2/ass2.md
@@ -0,0 +1,5 @@
+# Tiny Football
+
+- Hello
+- fsdf
+- sdfsf
--- ass2/assignment2.py
+++ ass2/assignment2.py
@@ -1,20 +1,68 @@
-import pygame, sys
-import pygame.time
+import pygame
+import sys
+import pygame.display
+import pygame.draw
 
-class GameState():
+class Ball:
+    def __init__(self) -> None:
+        self.speed_x = 1
+        self.speed_y = 1
+        pass
+    pass
+
+
+class GameState:
     def __init__(self):
         self.state = 'main_game'
+        self.speed_x = 5
+        self.speed_y = 10
 
-    def main_game(self):
+    def intro(self):
         pass
 
+    def main_game(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        ball.x += self.speed_x
+        ball.y += self.speed_y
+
+        if ball.top <= 0 or ball.bottom >= screen_h:
+            self.speed_y *= -1
+        if ball.left <= 0 or ball.right >= screen_w:
+            self.speed_x *= -1
+
+        screen.fill(LIGHT_GRAY)
+        pygame.draw.ellipse(screen, BLACK, ball)
+
     def state_manager(self):
+        if self.state == 'intro':
+            self.intro()
         if self.state == 'main_game':
             self.main_game()
-            pass
+
+WHITE = (255,255,255)
+LIGHT_GRAY = (200,200,200)
+BLACK = (0,0,0)
+BALL_RADIUS = 50
 
 pygame.init()
 clock = pygame.time.Clock()
 
+screen_w = 1280
+screen_h = 720
+screen = pygame.display.set_mode([screen_w, screen_h])
+ball = pygame.Rect(screen_w/2 - BALL_RADIUS, screen_h/2 - BALL_RADIUS, 2*BALL_RADIUS, 2*BALL_RADIUS)
+pygame.display.set_caption('Double Ball')
+
+
+
+game_state = GameState()
+
 while True:
+    game_state.state_manager()
+
+    pygame.display.flip()
     clock.tick(60)
