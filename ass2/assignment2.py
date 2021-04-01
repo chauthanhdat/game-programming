4d8dfe9eca50c040df47cacea5de33870c2d889e
Chau Thanh Dat <chauthanhdat1999@gmail.com>
2021-04-01 17:20:26 +0700

    add class ball

--- ass2/assignment2.py
+++ ass2/assignment2.py
@@ -2,20 +2,22 @@ import pygame
 import sys
 import pygame.display
 import pygame.draw
+import pygame.sprite
+import pygame.image
 
-class Ball:
-    def __init__(self) -> None:
-        self.speed_x = 1
-        self.speed_y = 1
-        pass
-    pass
-
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, speed) -> None:
+        super().__init__()
+        self.image = pygame.image.load('data/ball.png')
+        self.rect = pygame.Rect(screen_w/2 - BALL_RADIUS, screen_h/2 - BALL_RADIUS, 2*BALL_RADIUS, 2*BALL_RADIUS)
+        self.rect.x = screen_w/2
+        self.rect.y = screen_h/2
+        self.speed_x = speed[0]
+        self.speed_y = speed[1]
 
 class GameState:
     def __init__(self):
         self.state = 'main_game'
-        self.speed_x = 5
-        self.speed_y = 10
 
     def intro(self):
         pass
@@ -26,16 +28,18 @@ class GameState:
                 pygame.quit()
                 sys.exit()
 
-        ball.x += self.speed_x
-        ball.y += self.speed_y
-
-        if ball.top <= 0 or ball.bottom >= screen_h:
-            self.speed_y *= -1
-        if ball.left <= 0 or ball.right >= screen_w:
-            self.speed_x *= -1
+        for _ in ball_sprites:
+            if type(_) is Ball:
+                _.rect.x += _.speed_x
+                _.rect.y += _.speed_y
+                
+                if _.rect.top <= 0 or _.rect.bottom >= screen_h:
+                    _.speed_y *= -1
+                if _.rect.left <= 0 or _.rect.right >= screen_w:
+                    _.speed_x *= -1
 
         screen.fill(LIGHT_GRAY)
-        pygame.draw.ellipse(screen, BLACK, ball)
+        ball_sprites.draw(screen)
 
     def state_manager(self):
         if self.state == 'intro':
@@ -54,12 +58,12 @@ clock = pygame.time.Clock()
 screen_w = 1280
 screen_h = 720
 screen = pygame.display.set_mode([screen_w, screen_h])
-ball = pygame.Rect(screen_w/2 - BALL_RADIUS, screen_h/2 - BALL_RADIUS, 2*BALL_RADIUS, 2*BALL_RADIUS)
 pygame.display.set_caption('Double Ball')
 
-
-
 game_state = GameState()
+ball_sprites = pygame.sprite.Group()
+
+ball_sprites.add(Ball((1,2)), Ball((-2,-3)))
 
 while True:
     game_state.state_manager()
