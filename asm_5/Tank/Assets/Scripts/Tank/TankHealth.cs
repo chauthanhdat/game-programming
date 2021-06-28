2e8d7efa1f4ca142c5ed5f9be89e965fcaad9b1c
Chau Thanh Dat <chauthanhdat1999@gmail.com>
2021-06-29 00:20:56 +0700

    complete phase 7/8 tut - ass 5

--- asm_5/Tank/Assets/Scripts/Tank/TankHealth.cs
+++ asm_5/Tank/Assets/Scripts/Tank/TankHealth.cs
@@ -3,18 +3,18 @@ using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-    public float m_StartingHealth = 100f;          
-    public Slider m_Slider;                        
-    public Image m_FillImage;                      
-    public Color m_FullHealthColor = Color.green;  
-    public Color m_ZeroHealthColor = Color.red;    
+    public float m_StartingHealth = 100f;
+    public Slider m_Slider;
+    public Image m_FillImage;
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
     public GameObject m_ExplosionPrefab;
-    
-    
-    private AudioSource m_ExplosionAudio;          
-    private ParticleSystem m_ExplosionParticles;   
-    private float m_CurrentHealth;  
-    private bool m_Dead;            
+
+
+    private AudioSource m_ExplosionAudio;
+    private ParticleSystem m_ExplosionParticles;
+    private float m_CurrentHealth;
+    private bool m_Dead;
 
 
     private void Awake()
@@ -52,9 +52,9 @@ public class TankHealth : MonoBehaviour
     private void SetHealthUI()
     {
         // Adjust the value and colour of the slider.
-        m_Slider.value = m_CurrentHealth;
+        m_Slider.value = m_CurrentHealth/100;
 
-        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth/m_StartingHealth);
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
     }
 
 
@@ -66,7 +66,7 @@ public class TankHealth : MonoBehaviour
         m_ExplosionParticles.transform.position = transform.position;
         m_ExplosionParticles.gameObject.SetActive(true);
         m_ExplosionParticles.Play();
-        
+
         m_ExplosionAudio.Play();
 
         gameObject.SetActive(false);
